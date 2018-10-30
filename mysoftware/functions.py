bdc9c6111f8ec6421e90d4fbff1a627c7d33ce49
phmpm <mike.mercer87@gmail.com>
2018-10-30 16:32:39 +0000

    Added some missing files.

--- mysoftware/functions.py
+++ mysoftware/functions.py
@@ -1,6 +1,31 @@
 import numpy as np
 
 def square(x):
+    '''
+    Takes a number x and squares it.
+
+    Parameters:
+    -----------
+    x, float or int:
+        Number which is to be squared.
+
+    Returns:
+    --------
+    float:
+        Square of x
+
+    Examples:
+    ---------
+
+    >>> square(2)
+    4
+
+    >>> square(5)
+    25
+
+    >>> square(-1)
+    1
+    '''
     return x*x
 
 def oneoverr(x):
